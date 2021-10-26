5b114693b956768d4b058c8795687718cc2d10f5
huzaifa hassan <huzaifahassan97@hotmail.com>
2021-10-26 17:18:27 +0500

    Add project files.

---
+++ WebApplication1/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Areas.Identity.Data
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Lastname { get; set; }
+
+    }
+}
---
+++ WebApplication1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication1.Data;
+
+[assembly: HostingStartup(typeof(WebApplication1.Areas.Identity.IdentityHostingStartup))]
+namespace WebApplication1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DBContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+
+                })
+                    .AddEntityFrameworkStores<DBContext>();
+            });
+        }
+    }
+}
